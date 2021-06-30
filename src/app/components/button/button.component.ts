e8036543af6d79a4ca8a79c8ecb6349ce7bf56c8
BK <39931731+BitsBytesandLife@users.noreply.github.com>
2021-06-29 19:29:53 -0500

    add toggle form

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -16,6 +16,7 @@ export class ButtonComponent implements OnInit {
   }
 
   onClick(){
+    //console.log('Add');
     this.btnClick.emit();
   }
 
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from '../../services/ui.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,13 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Manager';
-  constructor() { }
+  showAddTask: boolean = false;
+  subscription: Subscription;
+  constructor(private uiService: UiService) { 
+    this.subscription = this.uiService
+    .onToggle()
+    .subscribe((value) => (this.showAddTask = value));
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask();
+    //console.log('toggle');
   }
 
 }
